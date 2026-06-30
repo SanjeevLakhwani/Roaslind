2f66de4d7bed06b52bb355e391996efe178e7ceb
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-06-30 14:18:41 -0400

    solve DNA nucleotide counting problem

--- dna/main.py
+++ dna/main.py
@@ -1,20 +1,26 @@
 import sys
 from pathlib import Path
+from collections import Counter
 
 INPUTS_DIR = Path(__file__).parent / "inputs"
 OUTPUTS_DIR = Path(__file__).parent / "outputs"
 
 
-def solve(dna: str) -> str:
-    # TODO: implement solution
-    return ""
+def solve_1(dna: str) -> str:
+    counts = {"A":0, "T":0, "G":0, "C":0}
+    for nt in dna:
+        counts[nt] += 1
+    return f"{counts['A']} {counts['C']} {counts['G']} {counts['T']}"
 
+def solve_2(dna: str) -> str:
+    c = Counter(dna)
+    return f"{c['A']} {c['C']} {c['G']} {c['T']}"
 
 def main(input_filename: str) -> None:
     input_path = INPUTS_DIR / input_filename
     dna = input_path.read_text().strip()
 
-    result = solve(dna)
+    result = solve_2(dna)
 
     stem = Path(input_filename).stem
     output_path = OUTPUTS_DIR / f"{stem}.output.txt"
