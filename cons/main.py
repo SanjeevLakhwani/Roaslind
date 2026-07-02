c0594b0354369e8a67ef5e8c34a3304bd38a4bb9
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-07-02 14:50:57 -0400

    solve cons, add grph scaffold

--- cons/main.py
+++ cons/main.py
@@ -1,9 +1,31 @@
-from utils import run
+from collections import defaultdict
+from utils import run, fasta_to_dict
 
+nts = ["A", "C", "G", "T"]
 
 def solve(data: str) -> str:
-    # TODO: implement solution
-    return ""
+    d = fasta_to_dict(data)
+    m = []
+    for r in d.values():
+        m.append(r)
+
+    out = ""
+
+    for j in range(len(m[0])):
+        c = defaultdict(int)
+        for i in range(len(m)):
+            c[m[i][j]] += 1
+
+        mn = ""
+        mv = 0
+        for nt in nts:
+            if mv < c[nt]:
+                mn = nt
+                mv = c[nt]
+
+        out += mn
+
+    return out
 
 
 if __name__ == "__main__":
---
+++ grph/main.py
@@ -0,0 +1,11 @@
+from utils import run, fasta_to_dict
+
+
+def solve(data: str, k: int = 3) -> str:
+    d = fasta_to_dict(data)
+    # TODO: implement solution
+    return ""
+
+
+if __name__ == "__main__":
+    run(solve)
