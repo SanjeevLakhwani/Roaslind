d536a01d43b3bfa95edad65b3c381855c8ce991e
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-07-02 00:32:45 -0400

    solve gc_content, add hamm scaffold

--- gc_content/main.py
+++ gc_content/main.py
@@ -10,8 +10,16 @@ def solve(data: str) -> str:
             d[ak] = ""
         else:
             d[ak] += l
-    print(d)
-    return ""
+
+    hk = ""
+    hp = 0
+    for k, v in d.items():
+        p = v.count("G") + v.count("C")
+        p /= len(v)
+        if p > hp:
+            hk, hp = k, p
+
+    return f"{hk}\n{hp*100}"
 
 
 if __name__ == "__main__":
