db85ed77d96ba6c5027fc32d05f8680cd49e9651
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-07-02 00:07:28 -0400

    solve fibd, add gc scaffold

--- fibd/main.py
+++ fibd/main.py
@@ -1,10 +1,22 @@
 from utils import run
 
 
-def solve(data: str) -> str:
-    n, m = map(int, data.split())
-    # TODO: implement solution
-    return ""
+def solve(data: str) -> int:
+    n, y = map(int, data.split())
+    imt = [0] * n
+    imt[0] = 1
+    m = 0
+    im = 1
+    for i in range(1, n):
+        lim = im
+        im_y_ago = 0 if i - y < 0 else imt[i-y]
+
+        im = m
+        imt[i] = im
+
+        m = lim + ( m - im_y_ago)
+
+    return m + im
 
 
 if __name__ == "__main__":
