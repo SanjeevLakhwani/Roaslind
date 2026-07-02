d5427db35e021da3be2d105d63a380dc83e29725
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-07-02 14:29:27 -0400

    solve subs, add cons scaffold

--- subs/main.py
+++ subs/main.py
@@ -3,9 +3,13 @@ from utils import run
 
 def solve(data: str) -> str:
     s, t = data.split()
-    # TODO: implement solution
-    return ""
+    lt = len(t)
+    out = ""
+    for i in range(len(s)-lt):
+        if s[i:i+lt] == t:
+            out+=f" {i+1}"
 
+    return out.lstrip()
 
 if __name__ == "__main__":
     run(solve)
