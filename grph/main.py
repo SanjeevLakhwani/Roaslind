5fbdedd1bd1c8e9df5923a8a9e819526c843792e
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-07-02 15:18:56 -0400

    solve grph, add iev scaffold

--- grph/main.py
+++ grph/main.py
@@ -3,8 +3,18 @@ from utils import run, fasta_to_dict
 
 def solve(data: str, k: int = 3) -> str:
     d = fasta_to_dict(data)
-    # TODO: implement solution
-    return ""
+    out = ""
+    for ok, ov in d.items():
+        if len(ov) < k:
+            continue
+        suf = ov[-k:]
+        for ik, iv in d.items():
+            if len(iv) < k or ov == iv:
+                continue
+            pre = iv[:k]
+            if suf == pre:
+                out += f"{ok} {ik}\n"
+    return out
 
 
 if __name__ == "__main__":
---
+++ iev/main.py
@@ -0,0 +1,12 @@
+from utils import run
+
+
+def solve(data: str) -> str:
+    n1, n2, n3, n4, n5, n6 = map(int, data.split())
+    # n1=AA-AA, n2=AA-Aa, n3=AA-aa, n4=Aa-Aa, n5=Aa-aa, n6=aa-aa
+    # TODO: implement solution
+    return ""
+
+
+if __name__ == "__main__":
+    run(solve)
