f0d7f818f365443c4e2310b4f132eea1e10a1a85
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-07-02 00:54:07 -0400

    solve hamm, add iprb scaffold

--- hamm/main.py
+++ hamm/main.py
@@ -1,11 +1,14 @@
 from utils import run
 
 
-def solve(data: str) -> str:
+def solve(data: str) -> int:
     s, t = data.split()
-    # TODO: implement solution
-    return ""
+    c = 0
+    for sbp, tbp in zip(s, t):
+        if sbp != tbp:
+            c += 1
 
+    return c
 
 if __name__ == "__main__":
     run(solve)
