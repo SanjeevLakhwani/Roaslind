3780de53ace05619694d2dd9257126f1b540686a
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-07-05 08:52:37 -0400

    solve perm, add prtm scaffold with monoisotopic mass table

--- perm/main.py
+++ perm/main.py
@@ -1,10 +1,22 @@
 from utils import run
 
+def perm(l:list) -> list :
+    if len(l) == 1:
+        return [l]
+    out = []
+    for i in range(len(l)):
+        for p in perm(l[:i]+l[i+1:]):
+            out.append([l[i], *p])
+    return out
+
 
 def solve(data: str) -> str:
     n = int(data.strip())
-    # TODO: implement solution
-    return ""
+
+    perms = perm(list(range(1,n+1)))
+    joined = [" ".join(str(x) for x in p) for p in perms]
+
+    return f"{len(perms)}\n{'\n'.join(joined)}"
 
 
 if __name__ == "__main__":
