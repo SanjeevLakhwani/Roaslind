c9e58b2f9f96dfa4ac0ec8d9e8dd8e3c12f6e115
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-07-05 03:08:41 -0400

    fix mprt extra blank line and UniProt URL, add mrna scaffold

--- mprt/main.py
+++ mprt/main.py
@@ -1,10 +1,11 @@
 import urllib.request
 from utils import run
+import re
 
 
 def fetch_fasta(uniprot_id: str) -> str:
     uid = uniprot_id.split("_")[0]
-    url = f"https://www.uniprot.org/uniprot/{uid}.fasta"
+    url = f"https://rest.uniprot.org/uniprotkb/{uid}.fasta"
     with urllib.request.urlopen(url) as r:
         lines = r.read().decode().splitlines()
     return "".join(lines[1:])
@@ -12,9 +13,15 @@ def fetch_fasta(uniprot_id: str) -> str:
 
 def solve(data: str) -> str:
     ids = data.split()
-    # TODO: implement solution
-    return ""
-
+    r = re.compile(r"(?=(N[^P][ST][^P]))")
+    out = []
+    for uid in ids:
+        seq = fetch_fasta(uid)
+        indexes = [m.start()+1 for m in r.finditer(seq)]
+        if indexes:
+            out.append(uid)
+            out.append(" ".join(map(str, indexes)))
+    return "\n".join(out)
 
 if __name__ == "__main__":
     run(solve)
---
+++ mrna/main.py
@@ -0,0 +1,12 @@
+from utils import run
+from codon_table import codons
+
+
+def solve(data: str) -> str:
+    protein = data.strip()
+    # TODO: implement solution (answer mod 1_000_000)
+    return ""
+
+
+if __name__ == "__main__":
+    run(solve)
